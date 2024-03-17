c05e191885d885b5cf743595b7fe886ee18935bd
mohito <mypywelcome@gmail.com>
2024-03-17 17:43:15 +0400

    taco-cloud

---
+++ src/test/java/com/mygitgor/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygitgor.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
