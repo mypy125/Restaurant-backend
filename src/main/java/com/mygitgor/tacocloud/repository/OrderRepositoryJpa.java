bb40a5855592e8d7b08fa3a4e98a2c4491ace634
mohito <mypywelcome@gmail.com>
2024-03-27 18:13:06 +0400

    adde_repositoryes

--- src/main/java/com/mygitgor/tacocloud/repository/OrderRepositoryJpa.java
+++ src/main/java/com/mygitgor/tacocloud/repository/OrderRepositoryJpa.java
@@ -1,11 +1,11 @@
 package com.mygitgor.tacocloud.repository;
 
+import org.springframework.data.domain.Pageable;
 import com.mygitgor.tacocloud.domain.taco.Order;
 import com.mygitgor.tacocloud.domain.taco.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 @Repository
 public interface OrderRepositoryJpa extends JpaRepository<Order, Long> {
