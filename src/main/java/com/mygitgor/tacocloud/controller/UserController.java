0da0bbba755e51d2ff532baf4c532bc4ce20bc35
mohito <mypywelcome@gmail.com>
2024-04-09 00:14:12 +0400

    add_userController

---
+++ src/main/java/com/mygitgor/tacocloud/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.mygitgor.tacocloud.controller;
+
+import com.mygitgor.tacocloud.domain.User;
+import com.mygitgor.tacocloud.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> findUserByJwtToken(@RequestHeader("Authorization") String jwt){
+        User user = userService.findUserByJwtToken(jwt);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
