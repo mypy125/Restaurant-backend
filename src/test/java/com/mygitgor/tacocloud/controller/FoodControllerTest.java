01cf249d6fca180816e08bea4c37b95779e7289f
mohito <mypywelcome@gmail.com>
2024-05-26 13:55:28 +0400

    add_FoodControllerTest

--- src/test/java/com/mygitgor/tacocloud/controller/FoodControllerTest.java
+++ src/test/java/com/mygitgor/tacocloud/controller/FoodControllerTest.java
@@ -54,4 +54,31 @@ public class FoodControllerTest {
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(foods, response.getBody());
     }
+
+    @Test
+    public void testGetRestaurantFood()throws Exception{
+        Long restaurantId = 1L;
+        boolean vegetarian = true;
+        boolean seasonal = true;
+        boolean nonVeg = false;
+        String foodCategory = "Starter";
+        String jwt = "dummy_jwt_token";
+
+        User user = new User();
+        user.setId(1L);
+
+        Food food1 = new Food();
+        food1.setName("Vegetarian Starter");
+        Food food2 = new Food();
+        food2.setName("Seasonal Salad");
+        List<Food> foods = Arrays.asList(food1, food2);
+
+        when(userService.findUserByJwtToken(jwt)).thenReturn(user);
+        when(foodService.getRestaurantFood(restaurantId, vegetarian, nonVeg, seasonal, foodCategory)).thenReturn(foods);
+
+        ResponseEntity<List<Food>> response = foodController.getRestaurantFood(vegetarian, seasonal, nonVeg, foodCategory, restaurantId, jwt);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(foods, response.getBody());
+    }
 }
