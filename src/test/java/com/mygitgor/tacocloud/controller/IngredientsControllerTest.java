9b5d39f243bd36141cd88e6480219031ff792df6
mohito <mypywelcome@gmail.com>
2024-05-28 13:24:23 +0400

    add_OrderControllerTest

--- src/test/java/com/mygitgor/tacocloud/controller/IngredientsControllerTest.java
+++ src/test/java/com/mygitgor/tacocloud/controller/IngredientsControllerTest.java
@@ -19,6 +19,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class IngredientsControllerTest {
